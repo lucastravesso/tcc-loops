c919c7f661e3bac91f1baeb6b3c4f7700f1b3f85
lucastravesso <lucasescobar.ti@hotmail.com>
2023-02-13 21:43:58 -0300

    estrutura for

--- C++/Loop/Pilha.cpp
+++ C++/Loop/Pilha.cpp
@@ -1,6 +1,12 @@
 #include "iostream"
+#include "stack"
 
 int main(){
-    
+    std::stack<int> pilha;
+
+    for (int i = 0; i < 200000000; ++i) {
+        pilha.push(i);
+    }
+
     return 0;
 }
