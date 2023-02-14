c4d9fb33ee0c747d3c83565227b320c6d5ebbfac
lucastravesso <lucasescobar.ti@hotmail.com>
2023-02-13 22:11:39 -0300

    refact dos tipos de lista

--- C++/Loop/Fila.cpp
+++ C++/Loop/Fila.cpp
@@ -1,12 +1,21 @@
+#include <chrono>
+#include <list>
 #include "iostream"
-#include "queue"
 
 int main(){
-    std::queue<int> fila;
+    std::list<int> fila;
+
+    auto horaDeInicio = std::chrono::system_clock::now();
 
     for (int i = 0; i < 200000000; ++i) {
-        fila.push(i);
+        fila.push_back(i);
     }
+
+    auto horaDeFim = std::chrono::system_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(horaDeFim - horaDeInicio);
+
+    std::cout << "Tempo " << duration.count() / 1000.0 << std::endl;
+
 
     return 0;
 }
--- C++/Loop/Pilha.cpp
+++ C++/Loop/Pilha.cpp
@@ -1,12 +1,18 @@
 #include "iostream"
 #include "stack"
+#include <chrono>
 
 int main(){
     std::stack<int> pilha;
+    auto horaDeInicio = std::chrono::system_clock::now();
 
     for (int i = 0; i < 200000000; ++i) {
         pilha.push(i);
     }
+    auto horaDeFim = std::chrono::system_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(horaDeFim - horaDeInicio);
+
+    std::cout << "Tempo " << duration.count() / 1000.0 << std::endl;
 
     return 0;
 }
