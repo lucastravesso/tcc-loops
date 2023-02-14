907b5bd02a9c4fdf953c0b4b1dcb4b4de71041d9
lucastravesso <lucasescobar.ti@hotmail.com>
2023-02-13 22:48:24 -0300

    loops em c++

--- C++/Loop/Fila.cpp
+++ C++/Loop/Fila.cpp
@@ -10,7 +10,7 @@ int main(){
     PROCESS_MEMORY_COUNTERS_EX pmc_start;
     GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&pmc_start, sizeof(pmc_start));
 
-    for (int i = 0; i < 200000000; ++i) {
+    for (int i = 0; i < 1000000; ++i) {
         fila.push_back(i);
     }
 
@@ -24,6 +24,81 @@ int main(){
 
     std::cout << "Memoria " << memory_used_end / 1024 / 1024 << std::endl;
     std::cout << "Tempo " << duration.count() / 1000.0 << std::endl;
+    fila.clear();
+
+    horaDeInicio = std::chrono::system_clock::now();
+    GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&pmc_start, sizeof(pmc_start));
+
+    for (int i = 0; i < 50000000; ++i) {
+        fila.push_back(i);
+    }
+
+    horaDeFim = std::chrono::system_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::milliseconds>(horaDeFim - horaDeInicio);
+
+    GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&pmc_end, sizeof(pmc_end));
+
+    memory_used_end = pmc_end.PrivateUsage;
+
+    std::cout << "Memoria " << memory_used_end / 1024 / 1024 << std::endl;
+    std::cout << "Tempo " << duration.count() / 1000.0 << std::endl;
+    fila.clear();
+
+    horaDeInicio = std::chrono::system_clock::now();
+    GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&pmc_start, sizeof(pmc_start));
+
+    for (int i = 0; i < 100000000; ++i) {
+        fila.push_back(i);
+    }
+
+    horaDeFim = std::chrono::system_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::milliseconds>(horaDeFim - horaDeInicio);
+
+    GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&pmc_end, sizeof(pmc_end));
+
+    memory_used_end = pmc_end.PrivateUsage;
+
+    std::cout << "Memoria " << memory_used_end / 1024 / 1024 << std::endl;
+    std::cout << "Tempo " << duration.count() / 1000.0 << std::endl;
+    fila.clear();
+
+    horaDeInicio = std::chrono::system_clock::now();
+    GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&pmc_start, sizeof(pmc_start));
+
+    for (int i = 0; i < 150000000; ++i) {
+        fila.push_back(i);
+    }
+
+    horaDeFim = std::chrono::system_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::milliseconds>(horaDeFim - horaDeInicio);
+
+    GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&pmc_end, sizeof(pmc_end));
+
+    memory_used_end = pmc_end.PrivateUsage;
+
+    std::cout << "Memoria " << memory_used_end / 1024 / 1024 << std::endl;
+    std::cout << "Tempo " << duration.count() / 1000.0 << std::endl;
+    fila.clear();
+
+    horaDeInicio = std::chrono::system_clock::now();
+    GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&pmc_start, sizeof(pmc_start));
+
+    for (int i = 0; i < 200000000; ++i) {
+        fila.push_back(i);
+    }
+
+    horaDeFim = std::chrono::system_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::milliseconds>(horaDeFim - horaDeInicio);
+
+    GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&pmc_end, sizeof(pmc_end));
+
+    memory_used_end = pmc_end.PrivateUsage;
+
+    std::cout << "Memoria " << memory_used_end / 1024 / 1024 << std::endl;
+    std::cout << "Tempo " << duration.count() / 1000.0 << std::endl;
+    fila.clear();
+
+
 
     return 0;
 }
--- C++/Loop/Pilha.cpp
+++ C++/Loop/Pilha.cpp
@@ -1,18 +1,82 @@
 #include "iostream"
 #include "stack"
 #include <chrono>
+#include <Windows.h>
+#include <psapi.h>
 
 int main(){
     std::stack<int> pilha;
     auto horaDeInicio = std::chrono::system_clock::now();
+    PROCESS_MEMORY_COUNTERS_EX pmc_end;
+    for (int i = 0; i < 1000000; ++i) {
+        pilha.push(i);
+    }
+    GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&pmc_end, sizeof(pmc_end));
+    SIZE_T memory_used_end = pmc_end.PrivateUsage;
+
+    auto horaDeFim = std::chrono::system_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(horaDeFim - horaDeInicio);
+
+    std::cout << "Memoria " << memory_used_end / 1024 / 1024 << std::endl;
+    std::cout << "Tempo " << duration.count() / 1000.0 << std::endl;
+
+    while (!pilha.empty()) pilha.pop();
+
+    for (int i = 0; i < 50000000; ++i) {
+        pilha.push(i);
+    }
+    GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&pmc_end, sizeof(pmc_end));
+    memory_used_end = pmc_end.PrivateUsage;
+
+    horaDeFim = std::chrono::system_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::milliseconds>(horaDeFim - horaDeInicio);
+
+    std::cout << "Memoria " << memory_used_end / 1024 / 1024 << std::endl;
+    std::cout << "Tempo " << duration.count() / 1000.0 << std::endl;
+
+    while (!pilha.empty()) pilha.pop();
+
+    for (int i = 0; i < 100000000; ++i) {
+        pilha.push(i);
+    }
+    GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&pmc_end, sizeof(pmc_end));
+    memory_used_end = pmc_end.PrivateUsage;
+
+    horaDeFim = std::chrono::system_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::milliseconds>(horaDeFim - horaDeInicio);
+
+    std::cout << "Memoria " << memory_used_end / 1024 / 1024 << std::endl;
+    std::cout << "Tempo " << duration.count() / 1000.0 << std::endl;
+
+    while (!pilha.empty()) pilha.pop();
+
+    for (int i = 0; i < 150000000; ++i) {
+        pilha.push(i);
+    }
+    GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&pmc_end, sizeof(pmc_end));
+    memory_used_end = pmc_end.PrivateUsage;
+
+    horaDeFim = std::chrono::system_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::milliseconds>(horaDeFim - horaDeInicio);
+
+    std::cout << "Memoria " << memory_used_end / 1024 / 1024 << std::endl;
+    std::cout << "Tempo " << duration.count() / 1000.0 << std::endl;
+
+    while (!pilha.empty()) pilha.pop();
 
     for (int i = 0; i < 200000000; ++i) {
         pilha.push(i);
     }
-    auto horaDeFim = std::chrono::system_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(horaDeFim - horaDeInicio);
+    GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&pmc_end, sizeof(pmc_end));
+    memory_used_end = pmc_end.PrivateUsage;
 
+    horaDeFim = std::chrono::system_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::milliseconds>(horaDeFim - horaDeInicio);
+
+    std::cout << "Memoria " << memory_used_end / 1024 / 1024 << std::endl;
     std::cout << "Tempo " << duration.count() / 1000.0 << std::endl;
+
+    while (!pilha.empty()) pilha.pop();
 
     return 0;
 }
