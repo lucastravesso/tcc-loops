faed8022fd707b2fc8f2f6b140e03893155783af
lucastravesso <lucasescobar.ti@hotmail.com>
2023-02-13 22:20:55 -0300

    uso de memoria

--- C++/Loop/Fila.cpp
+++ C++/Loop/Fila.cpp
@@ -1,11 +1,14 @@
 #include <chrono>
 #include <list>
+#include <Windows.h>
+#include <psapi.h>
 #include "iostream"
 
 int main(){
     std::list<int> fila;
-
     auto horaDeInicio = std::chrono::system_clock::now();
+    PROCESS_MEMORY_COUNTERS_EX pmc_start;
+    GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&pmc_start, sizeof(pmc_start));
 
     for (int i = 0; i < 200000000; ++i) {
         fila.push_back(i);
@@ -14,8 +17,13 @@ int main(){
     auto horaDeFim = std::chrono::system_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(horaDeFim - horaDeInicio);
 
-    std::cout << "Tempo " << duration.count() / 1000.0 << std::endl;
+    PROCESS_MEMORY_COUNTERS_EX pmc_end;
+    GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&pmc_end, sizeof(pmc_end));
 
+    SIZE_T memory_used_end = pmc_end.PrivateUsage;
+
+    std::cout << "Memoria " << memory_used_end / 1024 / 1024 << std::endl;
+    std::cout << "Tempo " << duration.count() / 1000.0 << std::endl;
 
     return 0;
 }
